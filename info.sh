36b029b9100a1b15d449cf009214e62877fec7fb
Jay Turla <shipcodez@gmail.com>
2017-06-06 06:18:28 +0800

    Update info.sh

--- info.sh
+++ info.sh
@@ -2,7 +2,7 @@
 # @shipcod3
 # credits goes to http://mazdatweaks.com/ for the wonderful Mazda hacks :)
 
-/jci/tools/jci-dialog --title="Shoutz to ROOTCON" --text="Jay Turla?" --ok-label='OK' --no-cancel
+/jci/tools/jci-dialog --title="Shoutz to ROOTCON" --text="Jay Turla will be improving this script soon" --ok-label='OK' --no-cancel
 
 sleep 10
 
