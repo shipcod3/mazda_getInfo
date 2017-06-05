147d170a37c0878d3565cdf77a89813e17880038
Jay Turla <shipcodez@gmail.com>
2017-06-06 05:07:59 +0800

    Create info.sh

---
+++ info.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+# @shipcod3
+# credits goes to http://mazdatweaks.com/ for the wonderful Mazda hacks :)
+
+/jci/tools/jci-dialog --title="Shoutz to ROOTCON" --text="Do you believe in true love?" --ok-label='OK' --no-cancel
+
+sleep 10
+
+killall jci-dialog
