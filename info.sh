495659906d6ac4400a1500992bcc2bccc99642a9
Jay Turla <shipcodez@gmail.com>
2022-05-24 00:17:01 +0800

    Update info.sh

--- info.sh
+++ info.sh
@@ -2,7 +2,7 @@
 # by @shipcod3
 # credits goes to http://mazdatweaks.com/ for the wonderful Mazda hacks since this is a known technique for CMU :)
 # more info: http://mazda3revolution.com/forums/2014-2017-mazda-3-skyactiv-audio-electronics/57714-infotainment-project.html
-# planning to compile some plug and play usb to RCE for other cars soon
+
 
 /jci/tools/jci-dialog --title="PoC" --text="A PoC that the USB port is an attack surface" --ok-label='OK' --no-cancel
 /jci/tools/jci-dialog --title="gr33tz" --text="To ROOTCON family" --ok-label='OK' --no-cancel
